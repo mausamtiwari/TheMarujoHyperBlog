34a42215b7f9180f47ef51857b18c726ac121d0d
Mausam <mausamtiwari93@gmail.com>
2024-05-22 19:40:03 +0200

    Added custom error page

--- src/main/java/be/intec/themarujohyperblog/model/User.java
+++ src/main/java/be/intec/themarujohyperblog/model/User.java
@@ -1,14 +1,21 @@
 package be.intec.themarujohyperblog.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
 
+@Getter
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true)
+   @NotNull
+   @Column(unique = true)
     private String firstname;
 
     @Column(nullable = false, unique = true)
@@ -23,52 +30,5 @@ public class User {
     @Column(nullable = false)
     private String email;
 
-    public Long getId() {
-        return id;
-    }
 
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFirstname() {
-        return firstname;
-    }
-
-    public void setFirstname(String firstname) {
-        this.firstname = firstname;
-    }
-
-    public String getLastname() {
-        return lastname;
-    }
-
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-// getters and setters
 }
