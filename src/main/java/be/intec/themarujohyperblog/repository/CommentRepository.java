9d1f638b43118f1251d3988a54dbe4042c73c17f
Jonathan <deroo.jon@gmail.com>
2024-05-23 15:38:25 +0200

    Merge remote-tracking branch 'origin/jonathan' into jonathan
    # Conflicts:
    #	src/main/java/be/intec/themarujohyperblog/repository/CommentRepository.java
    #	src/main/java/be/intec/themarujohyperblog/service/CommentServiceImpl.java

--- src/main/java/be/intec/themarujohyperblog/repository/CommentRepository.java
+++ src/main/java/be/intec/themarujohyperblog/repository/CommentRepository.java
@@ -5,9 +5,11 @@ import org.springframework.stereotype.Repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 @Repository
 public interface
 CommentRepository extends JpaRepository<BlogComment,Long> {
-    Page<BlogComment> findByPostId(Long blogPostId, Pageable pageable);
+    Page<BlogComment> findByBlogPostId(Long postId, Pageable pageable);
+
 }
