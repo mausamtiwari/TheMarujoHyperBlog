56bbed47d22d3bdfb5da21d2a032cb2abf4cd869
Jonathan <deroo.jon@gmail.com>
2024-05-29 16:10:41 +0200

    Met verbeterde blogcentral

--- src/main/java/be/intec/themarujohyperblog/service/PostService.java
+++ src/main/java/be/intec/themarujohyperblog/service/PostService.java
@@ -13,15 +13,14 @@ import org.springframework.data.domain.Pageable;
 import java.util.List;
 
 public interface PostService {
-    List<BlogPost> getAllPosts();
+
+
+
 
     BlogPost getPostById(Long blogPostId);
 
-    void savePost(BlogPost blogPost);
 
-    void deletePost(Long id);
 
-    Page<BlogPost> findPostPaginated(int pageNo, int pageSize);
 
     List<BlogPost> getPostsByUser(User user); // Add this method
 
@@ -31,8 +30,11 @@ public interface PostService {
 
     List<BlogPost> getAllPosts();
     void savePost(BlogPost post);
+
     BlogPost getPost(Long id);
+
     void deletePost(Long id);
+
     Page<BlogPost> findPostPaginated(int pageNo, int pageSize);
     Page<BlogPost> findPostPaginatedByIDUp(int pageNo, int pageSize);
     Page<BlogPost> findPostPaginatedByIDDown(int pageNo, int pageSize);
