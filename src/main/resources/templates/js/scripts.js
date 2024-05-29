9b439db5fcd47dfd0d08c6f60835b4c4522bd6bc
Jonathan <deroo.jon@gmail.com>
2024-05-29 14:47:52 +0200

    Met verbeterde blogcentral

--- src/main/resources/templates/js/scripts.js
+++ src/main/resources/templates/js/scripts.js
@@ -1,4 +1,4 @@
-/*!
+*!
 * Start Bootstrap - Blog Home v5.0.9 (https://startbootstrap.com/template/blog-home)
 * Copyright 2013-2023 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-blog-home/blob/master/LICENSE)
