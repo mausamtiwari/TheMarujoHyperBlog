9681709109134807a519a816cf6b1f7267675468
Rutger <rutgervandeborne@gmail.com>
2024-06-03 08:56:07 +0200

    Merge remote-tracking branch 'origin/rutger-2' into rutger-2

---
+++ src/main/java/be/intec/themarujohyperblog/repository/OnlineUserRepository.java
@@ -0,0 +1,15 @@
+package be.intec.themarujohyperblog.repository;
+
+import be.intec.themarujohyperblog.model.OnlineUser;
+import be.intec.themarujohyperblog.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface OnlineUserRepository extends JpaRepository<OnlineUser, Long> {
+    long count();
+    Optional<OnlineUser> findByUsername(String username);
+    Optional<OnlineUser> getOnlineUserById(Long userId);
+}
+
+
